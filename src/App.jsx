941d4e16864176ef0e889b6cb8425db378a45157
Bhargava-123 <bhargavashankar2003@gmail.com>
2024-01-02 11:23:38 +0530

    text-wrap animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect,useState } from 'react'
-import "./App.css"
+import "./App.scss"
 import analyticsIcon from "./assets/analytics.svg"
 import timer from "./assets/timer.svg"
 import todoList from "./assets/todoList.svg"
@@ -25,7 +25,7 @@ export const PanelHeader = () => {
 }
 
 export const CollapsedPanelHeader = () => {
-  return (<div className="panel-header">
+  return (<div className="panel-header collapse">
     <img src={timer} className="timer-icon-header" alt="" />
     <p id="Hub">
       H
@@ -44,33 +44,24 @@ export default function App() {
 
   const handleCollapse = () => {
     setCollapse(!collapse)
-    if (collapse === false) {
-      panelRef.current.style.width = "20rem"
-      setMenuItemClassName("menu-item");
-    }
-    else {
-      panelRef.current.style.width = "80px"
-      setMenuItemClassName("menu-item collapse");
-    }
-    
-    
   }
 
   return (
     <div className="App">
-      <div className="panel-container" ref = {panelRef}>
-        {!collapse ? <CollapsedPanelHeader></CollapsedPanelHeader> : <PanelHeader></PanelHeader>}
+      <div className={`panel-container ${!collapse ? "collapse" : ""}`} ref = {panelRef}>
+        <CollapsedPanelHeader></CollapsedPanelHeader>
+        <PanelHeader></PanelHeader>
         <div className="panel-content">
           <ul className='menu-list'>
-            <li className={menuItemClassName}>
+            <li className="menu-item">
               <img src={analyticsIcon} className='menu-icon' alt="" />
               <p>Analytics</p>
             </li>
-            <li className={menuItemClassName}>
+            <li className="menu-item">
               <img src={timer} className='menu-icon' alt="" />
               <p>Timer</p>
             </li>
-            <li className={menuItemClassName}>
+            <li className="menu-item">
               <img src={todoList} className='menu-icon' alt="" />
               <p>To-Do List</p>
             </li>
@@ -79,7 +70,7 @@ export default function App() {
         </div>
         <div className="panel-footer">
           <ul className="menu-list">
-            <li className={menuItemClassName}>
+            <li className="menu-item">
               <img src={settings} className='menu-icon' alt="" />
               <p>Setting</p>
             </li>
