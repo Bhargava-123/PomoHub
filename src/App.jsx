0bf4d1838197e445a7e759d7566a37ac879cd57c
Bhargava-123 <bhargavashankar2003@gmail.com>
2024-01-03 16:17:11 +0530

    new timer working

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect,useState } from 'react'
 import "./App.scss"
 import { PanelContextProvider } from './contexts/PanelContextProvider'
 import { SidePanel } from './components/SidePanel'
-import PomodoroTimer from './components/PomodoroTimer'
+import { PomodoroTimer } from './components/PomodoroTimer'
 import TimerContextProvider from './contexts/TimerContextProvider'
 import RightPanel from './components/RightPanel'
 
--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect } from 'react'
-import { useTimer } from 'react-timer-hook';
+import React, { useContext, useEffect,useState } from 'react'
 import pauseIcon from "../assets/pause.svg";
 import playIcon from "../assets/play.svg";
 import "../assets/css/PomodoroTimer.scss";
@@ -7,81 +6,30 @@ import stopIcon from "../assets/stop.svg";
 
 //WHAT TO DO WHEN THE TIMER ENDS ? RESET THE ORIGINAL EXPIRY TIME / GO TO BREAK /
 
-import { useState } from 'react';
-function MyTimer({ expiryTimestamp, workTime }) {
+import { useTimer } from '@mzaleski/use-timer';
 
-
-    const {
-        minutes,
-        seconds,
-        pause,
-        resume,
-        restart,
-    } = useTimer({ expiryTimestamp, onExpire: () => setTimerState("pause"),autoStart:false});
-
-
-    const [timerState, setTimerState] = useState("pause");
-
-    useEffect(() => {
-        timerState === "resume" ? resume() : pause();
-        console.log(expiryTimestamp);
-    },[timerState])
-
-    const handleTimerState = () => {
-        if (timerState == "pause") {
-            setTimerState("resume");
-        }
-        else {
-            setTimerState("pause");
-        }
-    }
-
-    const handleRestart = () => {
-        const time = new Date();
-        time.setMinutes(time.getMinutes() + workTime);
-        restart(time, false);
-        setTimerState("pause");
-    }
-
-    return (
-        <div >
-            <div className="timer-container">
-                <div className="minutes-container">
-                    {minutes.toString().padStart(2,'0')}
-                </div>
-                <div className="button-container">
-                    <img src={timerState === "pause" ? playIcon : pauseIcon} alt="" onClick={handleTimerState} className='pause-play-icon' />
-                    <img src={stopIcon} alt="" onClick={handleRestart} className='pause-play-icon'/>
-
-                </div>
-                <div className="seconds-container">
-                    {seconds.toString().padStart(2, '0')}
-                </div>
-            </div>
-            
-            
-            {/* <button onClick={() => pause()}>Pause</button>
-             */}
-            {/* <button onClick={() => {
-                // Restarts to 5 minutes timer
-                const time = new Date();
-                time.setSeconds(0);
-                restart(time,false)
-            }}>Reset</button> */}
-        </div>
+export const PomodoroTimer = () => {
+    const { timeRemaining, secondsRemaining, setFreeze, resetTimer,isFrozen } = useTimer(65, false,
+        () => console.log('Timer finished!')
     );
-}
 
-export default function PomodoroTimer() {
-    const time = new Date();
-
-    const workTime = 10;
-
-    time.setMinutes(time.getMinutes()+workTime);
-    console.log(time);
+    let timeObj = new Date(secondsRemaining * 1000);
+    var minutes = timeObj.getUTCMinutes()
+    var seconds = timeObj.getSeconds();
+    console.log(minutes.toString().padStart(2, '0') + ":" + seconds.toString().padStart(2, '0'));
     return (
-        <div className='timer-content-container'>
-            <MyTimer expiryTimestamp={time} workTime = {workTime} />
+        <div className='timer-container=container'>
+            <div className="timer-container">
+                <div>{minutes.toString().padStart(2,'0')}</div>
+                <div className="button-container">
+                    <div className="pause-play-icon">
+
+                        {/* <img src={isFrozen ? playIcon : pauseIcon} alt=""  />
+                        <img src={stopIcon} alt="" /> */}
+                    </div>
+                </div>
+                <div>{ minutes.toString().padStart(2,'0') }</div>
+            </div>
         </div>
     );
 }
