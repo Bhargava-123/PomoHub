d1806e7cb5c31eb622071d983b1f1d57ce9396ff
Bhargava-123 <bhargavashankar2003@gmail.com>
2024-01-05 03:42:29 +0530

    todolist strike throughh done

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,45 +1,71 @@
-import React, { useContext,useRef } from 'react'
+import React, { forwardRef, useContext,useRef } from 'react'
 import "../assets/css/TodoList.scss"
 import { PanelContext } from '../contexts/PanelContextProvider'
 import checkBoxEmpty from "../assets/todo-list-logos/checkBoxEmpty.svg"
 import checkBoxChecked from "../assets/todo-list-logos/checkBoxChecked.svg"
+
+
+export const Todo = ({ handleCheckBox, taskName }) => {
+    
+    const todoContainerRef = useRef();
+
+    return (
+        <div className="todo-container empty" ref = {todoContainerRef}>
+            <div className="checkBox-container" onClick={() => handleCheckBox(todoContainerRef)}>
+                <img src={checkBoxEmpty} alt="" className='checkBox' />
+            </div>
+            <div className="todo-task-name">
+                {taskName}
+            </div>
+        </div>
+   )
+}
+
+
 export default function TodoList() {
 
     const { collapse } = useContext(PanelContext);
-    const checkBoxContainerRef = useRef();
+    const todoContainerRef = useRef([]);
 
-    const handleCheckBox = () => {
-        const checkBoxEle = checkBoxContainerRef.current.children[0]
-        if (checkBoxContainerRef.current.className.includes("empty")) {
+    const handleCheckBox = (todoContainerRef) => {
+        console.log(todoContainerRef.current); 
+        console.log(todoContainerRef.current.children[0].children[0]);
+        const checkBoxEle = todoContainerRef.current.children[0].children[0]
+        if (todoContainerRef.current.className.includes("empty")) {
             checkBoxEle.src = checkBoxChecked
-            checkBoxContainerRef.current.className = "checkBox-container checked";
+            todoContainerRef.current.className = "todo-container checked";
         }
         else{
             checkBoxEle.src = checkBoxEmpty
-            checkBoxContainerRef.current.className = "checkBox-container empty"
+            todoContainerRef.current.className = "todo-container empty"
         }
     }
 
+    const todoTaskList = [
+        {
+            taskName: "Study Compiler Design",
+            isCompleted: false,
+
+        },
+        {
+            taskName: "Study Crypto",
+            isCompleted: true,
+        },
+    ]
+
         return (
-            <div
-                className={`todo-list-container ${!collapse ? "collapse" : ""}`}>
-                <div className="todo-container" >
-                    <div className="checkBox-container empty" ref={checkBoxContainerRef} onClick={handleCheckBox}>
-                        <img src={checkBoxEmpty} alt="" className='checkBox' />
-                    </div>
-                    <div className="todo-task-name">
-                        study vcc
-                    </div>
-                </div>
-                <div className="todo-container">
-                    <div className="checkBox-container">
-                        <img src={checkBoxEmpty} alt="" className='checkBox' />
-                    </div>
-                    <div className="todo-task-name">
-                        study css
-                    </div>
-                </div>
-      
+            <div className={`todo-list-container ${!collapse ? "collapse" : ""}`}>
+                {
+                    todoTaskList.map((value, key) => {
+                        return (
+                            <Todo
+                                taskName={value.taskName}
+                                handleCheckBox={handleCheckBox}
+                            ></Todo>
+                        )
+                    })
+                }
+                
             </div>
         )
 }
